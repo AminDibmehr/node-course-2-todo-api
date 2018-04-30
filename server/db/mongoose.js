d02ad27e8d4be623941d8437941286d9e3c96746
amin <dibmehrm@gmail.com>
2018-04-30 13:38:39 +0430

    add delete /todos/id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://dibmehrm:se1234amin@ds163119.mlab.com:63119/todoapp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
+
+
+// 'mongodb://dibmehrm:se1234amin@ds163119.mlab.com:63119/todoapp'
