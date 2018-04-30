91538114dbecf5346c64073d7aab1ff265bb2805
amin <dibmehrm@gmail.com>
2018-04-30 09:42:43 +0430

    update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://dibmehrm:se1234amin@ds163119.mlab.com:63119/todoapp');
 
 module.exports = {mongoose};
